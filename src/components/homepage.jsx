d620c12682df89c3613cc4f176054e53338205f7
KKSAGAR08 <kksagar08062004@gmail.com>
2025-04-13 18:17:57 +0530

    Plant Disease Detection

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 import { useNavigate } from "react-router-dom"
+import { OrbitProgress } from 'react-loading-indicators';
 import axios from "axios";
 
 function Homepage() {
 
     const [img, setImg] = useState(null);
     const [predict, setPredict] = useState(null);
-    const [imageURL, setImageURL] = useState(null)
+    const [imageURL, setImageURL] = useState(null);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     function handleChange(e) {
@@ -30,6 +32,7 @@ function Homepage() {
         formdata.append("file", img);
 
         try {
+            setLoading(true);
             const response = await axios.post("http://localhost:5000/predict", formdata, {
                 headers: { "Content-Type": "multipart/form-data" }
             });
@@ -47,6 +50,9 @@ function Homepage() {
             console.error("Error connecting to Flask");
             alert("Error connecting to flask");
         }
+        finally{
+            setLoading(false); 
+        }
     };
 
     return (
@@ -60,6 +66,11 @@ function Homepage() {
                         <input type="file" onChange={handleChange} className="form-control" accept="image/*" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload" />
                         <button className="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04" onClick={handleSubmit}>Button</button>
                     </div>
+                    {loading && (
+                    <div className="d-flex justify-content-center mt-4">
+                        <OrbitProgress color="#32cd32" size="medium" text="" textColor="" />
+                    </div>
+                    )}
                 </div>
             </div>
         </>
