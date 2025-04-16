09f2de89257e4cc266962fc4e83912e1beaa220f
KKSAGAR08 <kksagar08062004@gmail.com>
2025-04-16 17:14:35 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
+import os  # Import os for model path handling
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
